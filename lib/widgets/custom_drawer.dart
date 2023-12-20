ff4dc34fde6bebe30e6f2becbedb7493ef7392d7
KingNarmar <adlymina99@gmail.com>
2023-12-20 23:21:31 +0200

    mobile layout done

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,21 +1,32 @@
+import 'package:adaptaive_app/models/drawer_item_model.dart';
 import 'package:flutter/material.dart';
+
+import 'custom_drawer_items_list_view.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-
+  final List<DrawerItemModel> items = const [
+    DrawerItemModel(title: "DASHBOARD", icon: Icons.home),
+    DrawerItemModel(title: "SETTINGS", icon: Icons.settings),
+    DrawerItemModel(title: "ABOUT", icon: Icons.info),
+    DrawerItemModel(title: "LOGOUT", icon: Icons.logout),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
-      backgroundColor: Color(0xffD6D2D9),
-      child: Column(
-        children: [
-          DrawerHeader(
-            child: Icon(
-              Icons.favorite,
-              size: 48,
+    return Drawer(
+      backgroundColor: const Color(0xffD6D2D9),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const DrawerHeader(
+              child: Icon(
+                Icons.favorite,
+                size: 48,
+              ),
             ),
-          ),
-        ],
+            CustomeDrawerItemsListView(items: items)
+          ],
+        ),
       ),
     );
   }
