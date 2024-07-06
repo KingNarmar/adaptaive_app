17de80310df940abca9abaae3d1f550d2356fca2
KingNarmar <adlymina99@gmail.com>
2024-07-06 17:52:41 +0300

    Video 16 Simple refactor

--- lib/screens/widgets/custom_sliver_grid.dart
+++ lib/screens/widgets/custom_sliver_grid.dart
@@ -1,3 +1,4 @@
+import 'package:adaptaive_app/screens/widgets/custom_item.dart';
 import 'package:flutter/material.dart';
 
 class CustomSliverGrid extends StatelessWidget {
@@ -13,11 +14,7 @@ class CustomSliverGrid extends StatelessWidget {
       ),
       itemCount: 4,
       itemBuilder: (context, index) {
-        return Container(
-          decoration: BoxDecoration(
-              color: const Color(0xffB5B4B6),
-              borderRadius: BorderRadius.circular(8)),
-        );
+        return const CustomItem();
       },
     );
   }
