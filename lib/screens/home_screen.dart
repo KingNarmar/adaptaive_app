7281bdd23039d97921b5ce4e1f8666423d9489f1
KingNarmar <adlymina99@gmail.com>
2024-07-06 17:09:23 +0300

    Start From the begining

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,36 +1,10 @@
-import 'package:adaptaive_app/widgets/custom_drawer.dart';
-import 'package:adaptaive_app/widgets/home_screen_body.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      drawer: const CustomDrawer(),
-      backgroundColor: const Color(0xffDCDADC),
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            scaffoldKey.currentState!.openDrawer();
-          },
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.black,
-      ),
-      body: const HomeScreenBody(),
-    );
+    return const Placeholder();
   }
 }
