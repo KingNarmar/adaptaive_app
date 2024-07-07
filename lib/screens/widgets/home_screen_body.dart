a63a517ccaafe2e2ea197678fd2fe80384ecb542
KingNarmar <adlymina99@gmail.com>
2024-07-07 23:24:31 +0300

    Video 36 Adaptive Layout Part One “Code refactor”

--- lib/screens/widgets/home_screen_body.dart
+++ lib/screens/widgets/home_screen_body.dart
@@ -1,28 +1,23 @@
+import 'package:adaptaive_app/screens/widgets/desktop_layout.dart';
+import 'package:adaptaive_app/screens/widgets/mobile_layout.dart';
 import 'package:adaptaive_app/screens/widgets/tablet_layout.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import 'desktop_layout.dart';
-import 'mobile_layout.dart';
+import 'adaptive_layout.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth < 600) {
-            return const MobileLayout();
-          } else if (constraints.maxWidth < 900) {
-            return const TabletLayout();
-          } else {
-            return const DesktopLayout();
-          }
-        },
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: AdaptiveLayout(
+        mobileLayout: MobileLayout(),
+        tabletLayout: TabletLayout(),
+        desktopLayout: DesktopLayout(),
       ),
     );
   }
