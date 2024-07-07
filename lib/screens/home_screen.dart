6b62486d580ec13c074571b44be0c9caa478438a
KingNarmar <adlymina99@gmail.com>
2024-07-07 13:06:30 +0300

    Video 18 Open Drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,30 @@
+import 'package:adaptaive_app/screens/widgets/custom_drawer.dart';
 import 'package:adaptaive_app/screens/widgets/home_screen_body.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: const CustomDrawer(),
       backgroundColor: const Color(0xffDCDBDC),
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: const Icon(
-          Icons.menu,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            scaffoldKey.currentState!.openDrawer();
+          },
           color: Colors.white,
         ),
       ),
