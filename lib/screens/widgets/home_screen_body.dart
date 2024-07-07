c2378906e11946b534d76dbe1ea92e62298d9dcb
KingNarmar <adlymina99@gmail.com>
2024-07-07 17:25:30 +0300

    Video 24 Tablet layout &Video 25 Tablet breakpoint

--- lib/screens/widgets/home_screen_body.dart
+++ lib/screens/widgets/home_screen_body.dart
@@ -1,23 +1,35 @@
 import 'package:adaptaive_app/screens/widgets/custom_sliver_grid.dart';
 import 'package:adaptaive_app/screens/widgets/custom_sliver_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(
+          const SliverToBoxAdapter(
             child: SizedBox(
               height: 16,
             ),
           ),
-          CustomSliverGrid(),
-          CustomSliverList()
+          SliverToBoxAdapter(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                if (constraints.maxWidth > 600) {
+                  return const Text("Tablet Layout");
+                } else {
+                  return const CustomSliverGrid();
+                }
+              },
+            ),
+          ),
+          const CustomSliverList()
         ],
       ),
     );
